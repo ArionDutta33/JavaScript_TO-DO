0c36a3740642759b320c2dbd8a5a729a94eb6929
Arion Dutta <146577817+ArionDutta33@users.noreply.github.com>
2024-03-20 23:56:29 +0530

    Update app.js
    Added new features-
    1. Now you could delete a particular task by clicking on the delete button.
    2. You can mark whether a task has been completed or not by clicking on the check  icon. There will be a line through when clicked indicating the task has been completed.
    You can also undo this  by clicking on the check again.
    2. Made the webpage responsive to various devices like mobiles, tablets etc.

--- app.js
+++ app.js
@@ -5,17 +5,59 @@ const taskContainer = document.querySelector(".task-container")
 const taskCounter = document.querySelector(".show-task")
 
 checkBtn.addEventListener("click", () => {
-    const newTask = document.createElement("div")
-    newTask.textContent = inputTask.value
-    newTask.classList.add("task-show")
-    taskContainer.appendChild(newTask)
+
+    if (inputTask.value === "")
+        alert("pls enter a task")
+    else {
+        const newTask = document.createElement("div")
+        newTask.textContent = inputTask.value
+        newTask.classList.add("task-show")
+
+        taskContainer.appendChild(newTask)
 
 
-    //also create the delete btn
-    const delBtn = document.createElement("button")
-    delBtn.classList.add(".dl_clr")
-    delBtn.innerHTML = '<i class="fa-solid fa-trash"></i>'
-    taskContainer.appendChild(delBtn)
+        //also create the delete btn
+        const delBtn = document.createElement("button")
+        delBtn.classList.add("delete")
+        // delBtn.innerHTML = '<i class="fa-solid fa-trash"></i>'
+        delBtn.innerHTML = 'Delete'
+        newTask.appendChild(delBtn)
 
+        //creating the check tbn
+        const doneBtn = document.createElement("button")
+        doneBtn.classList.add("done")
+        doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>'
+        newTask.appendChild(doneBtn)
+
+        //strike through if a task has been completed
+        let flag = 1;
+        doneBtn.addEventListener("click", (e) => {
+            let target = e.target
+            // console.log("hello")
+            // console.log(target.parentElement)
+
+            if (flag) {
+                target.parentElement.parentElement.style.textDecoration = "line-through"
+                flag = 0
+            }
+            else {
+                target.parentElement.parentElement.style.textDecoration = "none"
+                flag = 1
+            }
+
+        })
+
+        //removing a task 
+        delBtn.addEventListener("click", (e) => {
+
+            let target = e.target
+            if (target === delBtn)
+
+                target.parentElement.remove()
+
+            console.log("done")
+        })
+        inputTask.value = ""
+    }
 
 })
